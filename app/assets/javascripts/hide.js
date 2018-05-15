6458a14d67ef759783da32376b09dd0527508b2c
Efrain Perez Jr <efrainperezjr@live.com>
2018-05-15 20:04:20 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,13 +1,4 @@
-function hideWhenClicked() {
-  const hideThisDiv = document.getElementById('hide_this');
-  hideThisDiv.addEventListener('click', (event) => {
-    hideThisDiv.innerHTML = "";
-  });
-}
-
 function hideLink() {
   const hideThisDiv = document.getElementById('hide_this');
   hideThisDiv.innerHTML = "";
 }
-
-hideWhenClicked();
